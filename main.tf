5ff3bbcae656833960ad4a31f8a0783dd0fa0081
jagadish-ctl <jagadish.ganesh@lumen.com>
2026-03-13 12:53:51 +0530

    Initial Terraform networking config

---
+++ main.tf
@@ -0,0 +1,82 @@
+provider "google" {
+  region  = var.region
+}
+
+# Create new GCP project
+resource "google_project" "networkingglobal" {
+  name            = "Networkingglobal"
+  project_id      = var.new_project_id
+  billing_account = var.billing_account
+}
+
+# Enable required Google Cloud APIs
+resource "google_project_service" "compute_api" {
+  project = google_project.networkingglobal.project_id
+  service = "compute.googleapis.com"
+  
+  disable_on_destroy = false
+}
+
+# Create the VPC network named "vpc_networkingVPN"
+resource "google_compute_network" "vpc_networkingVPN" {
+  project                 = google_project.networkingglobal.project_id
+  name                    = "vpc_networkingVPN"
+  auto_create_subnetworks = false
+  
+  depends_on = [google_project_service.compute_api]
+}
+
+# Create subnet named "subnet-vpn" with CIDR 20.20.0.0/16
+resource "google_compute_subnetwork" "subnet_vpn" {
+  project = google_project.networkingglobal.project_id
+
+  name = "subnet-vpn"
+
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc_networkingVPN.id
+
+  private_ip_google_access = true
+}
+
+# Firewall rule to allow IAP (Identity-Aware Proxy) access
+resource "google_compute_firewall" "allow_iap" {
+  project = google_project.networkingglobal.project_id
+  name    = "allow-iap-ssh"
+  network = google_compute_network.vpc_networkingVPN.name
+  
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  
+  source_ranges = ["35.235.240.0/20"]
+  
+  description = "Allow SSH access from Google Cloud IAP"
+}
+
+# Firewall rule to allow Firezone/WireGuard UDP traffic
+resource "google_compute_firewall" "allow_firezone_udp" {
+  project = google_project.networkingglobal.project_id
+  name    = "allow-firezone-udp"
+  network = google_compute_network.vpc_networkingVPN.name
+  
+  allow {
+    protocol = "udp"
+    ports    = ["51820"]
+  }
+  
+  source_ranges = ["0.0.0.0/0"]
+  
+  description = "Allow WireGuard VPN traffic on standard port 51820"
+}
+
+# VPC Peering from Hub to Spoke
+resource "google_compute_network_peering" "hub_to_spoke" {
+  name         = "vpc_networkingVPN-to-Jenkins-private"
+  network      = google_compute_network.vpc_networkingVPN.self_link
+  peer_network = "projects/core-it-infra-prod/global/networks/Jenkins-private"
+  
+  export_custom_routes = true
+  import_custom_routes = true
+}
